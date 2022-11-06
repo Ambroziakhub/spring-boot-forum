6fea2054572343bdccd37e95b98b5df1cb65a2d0
rafalstef <r.stefanowicz1@gmail.com>
2022-11-06 08:51:34 +0100

    Email service to send confirmation email to registered user.

---
+++ src/main/java/dev/forum/forum/email/EmailDetails.java
@@ -0,0 +1,18 @@
+package dev.forum.forum.email;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class EmailDetails {
+    private String recipient;
+    private String msgBody;
+    private String subject;
+    private String attachment;
+}
